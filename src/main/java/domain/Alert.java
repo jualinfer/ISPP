36d5d4824722604ffc774f2cd1ed274e80bdd625
Curro Márquez <curro171093@gmail.com>
2019-04-28 12:29:10 +0200

    Basic Alert classes

---
+++ src/main/java/domain/Alert.java
@@ -0,0 +1,87 @@
+
+package domain;
+
+import java.util.Date;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Access(AccessType.PROPERTY)
+public class Alert extends DomainEntity {
+
+	//Attributes
+
+	private Date		date;
+	private Actor		sender;
+	private Actor		receiver;
+	private TypeAlert	typeAlert;
+	private Boolean		isRead;
+	//Relationships
+
+	private Route		relatedRoute;
+
+
+	//Getters
+
+	@Valid
+	@NotNull
+	public Route getRelatedRoute() {
+		return this.relatedRoute;
+	}
+
+	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
+	@DateTimeFormat(pattern = "dd/MM/yyyy HH:mm")
+	public Date getDate() {
+		return this.date;
+	}
+
+	@NotNull
+	public Actor getSender() {
+		return this.sender;
+	}
+
+	@NotNull
+	public Actor getReceiver() {
+		return this.receiver;
+	}
+
+	@NotNull
+	public Boolean getIsRead() {
+		return this.isRead;
+	}
+
+	public TypeAlert getTypeAlert() {
+		return this.typeAlert;
+	}
+
+	//Setters
+
+	public void setDate(final Date date) {
+		this.date = date;
+	}
+
+	public void setSender(final Actor sender) {
+		this.sender = sender;
+	}
+
+	public void setReceiver(final Actor receiver) {
+		this.receiver = receiver;
+	}
+
+	public void setTypeAlert(final TypeAlert typeAlert) {
+		this.typeAlert = typeAlert;
+	}
+
+	public void setIsRead(final Boolean isRead) {
+		this.isRead = isRead;
+	}
+}
