b36304b573511c45d777f9a70ac1c5d7ada5c26c
Curro Márquez <curro171093@gmail.com>
2019-04-30 19:32:40 +0200

    Alert edit controller

--- src/main/java/controllers/AlertController.java
+++ src/main/java/controllers/AlertController.java
@@ -3,8 +3,12 @@ package controllers;
 
 import java.util.Collection;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -63,6 +67,43 @@ public class AlertController extends AbstractController {
 		return result;
 	}
 
+	// Edit -------------------------------------
+
+	@RequestMapping(value = "/edit", method = RequestMethod.GET)
+	public ModelAndView edit(@RequestParam final int alertId) {
+		ModelAndView result;
+		Alert alert;
+
+		alert = this.alertService.findOne(alertId);
+
+		result = this.createEditModelAndView(alert);
+
+		return result;
+	}
+
+	//Save--------------------------------
+
+	@RequestMapping(value = "/edit", method = RequestMethod.POST, params = "save")
+	public ModelAndView save(@Valid final Alert alert, final BindingResult binding) {
+		ModelAndView result;
+		Alert saved;
+
+		for (final ObjectError asd : binding.getAllErrors())
+			System.out.println(asd);
+
+		if (binding.hasErrors())
+			result = this.createEditModelAndView(alert);
+		else
+			try {
+				saved = this.alertService.save(alert);
+				result = new ModelAndView("redirect:/alert/driver/list.do");
+			} catch (final Throwable oops) {
+				oops.printStackTrace();
+				result = this.createEditModelAndView(alert, "alert.commit.error");
+			}
+
+		return result;
+	}
 	//Seen
 	@RequestMapping(value = "/alertSeen", method = RequestMethod.GET)
 	public ModelAndView alertSeen(@RequestParam final int varId) {
@@ -83,5 +124,23 @@ public class AlertController extends AbstractController {
 
 		return result;
 	}
+	//Ancilliary methods
 
+	private ModelAndView createEditModelAndView(final Alert alert) {
+		ModelAndView result;
+
+		result = this.createEditModelAndView(alert, null);
+
+		return result;
+	}
+
+	private ModelAndView createEditModelAndView(final Alert alert, final String message) {
+		ModelAndView result;
+
+		result = new ModelAndView("alert/administrator/edit");
+		result.addObject("alert", alert);
+		result.addObject("message", message);
+
+		return result;
+	}
 }
