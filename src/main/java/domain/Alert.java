e2f4285564a1d4a0930644edc1f23b25d59d38eb
jualinfer <jualinfer1@alum.us.es>
2019-05-01 17:55:43 +0200

    Bugfix alerts, #66
    - Fixed relation declaration between alerts and actors.

--- src/main/java/domain/Alert.java
+++ src/main/java/domain/Alert.java
@@ -7,7 +7,6 @@ import javax.persistence.Access;
 import javax.persistence.AccessType;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.Valid;
@@ -50,14 +49,14 @@ public class Alert extends DomainEntity {
 
 	@NotNull
 	@Valid
-	@OneToOne(optional = false)
+	@ManyToOne(optional = false)
 	public Actor getSender() {
 		return this.sender;
 	}
 
 	@NotNull
 	@Valid
-	@OneToOne(optional = false)
+	@ManyToOne(optional = false)
 	public Actor getReceiver() {
 		return this.receiver;
 	}
